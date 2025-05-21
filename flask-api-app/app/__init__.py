600c62f9087935476cd62117b1e4cb8aacd142af
Clarissa Baciu <clarissa.baciu@mail.mcgill.ca>
2025-05-21 18:39:53 +0000

    Created transaction model

--- flask-api-app/app/__init__.py
+++ flask-api-app/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from app.routes import api
 
 app = Flask(__name__)
+app.register_blueprint(api)
 
 from app import routes, models
--- flask-api-app/app/models.py
+++ flask-api-app/app/models.py
@@ -1,10 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-class ExampleModel(db.Model):
+class TransactionType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+class TransactionModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    cost = db.Column(db.Float, nullable=False)
+    type_id = db.Column(db.Integer, db.ForeignKey('transaction_type.id'), nullable=False)
+    transaction_type = db.relationship('TransactionType', backref=db.backref('transactions', lazy=True))
 
     def __repr__(self):
-        return f'<ExampleModel {self.name}>'
+        return f'<TransactionModel id={self.id} date={self.date} cost={self.cost} type={self.transaction_type.name}>'
+    
